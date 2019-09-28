436b458bffec195a0ac7e97a15d67a0b06de5d05
wagnercandido <wagnercssilva@gmail.com>
2019-09-27 23:29:11 -0300

    MaterialModeule --comented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app.routing.module';
 import { NgModule } from '@angular/core';
+// import { MaterialModule } from './Util/material.module';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { DashboardModule } from '../app/Features/dashboard/dashboard.module';
 import { LoginModule } from '../app/Features/login/login.module';
@@ -15,6 +17,8 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     LoginModule,
     DashboardModule,
+    // BrowserAnimationsModule,
+    // MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
