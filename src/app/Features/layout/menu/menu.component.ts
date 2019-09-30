bb673a74047e0895425b082c9c5984674f7b146a
Wagner Candido da Silva <wcsilva@latam.stefanini.com>
2019-09-30 17:04:34 -0300

    Menu: Personalizando links e rotas

--- src/app/Features/layout/menu/menu.component.ts
+++ src/app/Features/layout/menu/menu.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
+  options = [
+    { icon: 'home', title: 'Dashboard', router: 'principal' }
+    { icon: 'swap_vert', title: 'Transações', router: 'outro' }
+    { icon: 'account_balance', title: 'Contas', router: 'contas' }
+    { icon: 'credit_card', title: 'Cartões', router: 'cartoes' }
+    { icon: 'insert_chart_outlined', title: 'Relatórios', router: 'relatorios' }
+    { icon: 'bookmarks', title: 'Categorias', router: 'categorias' }
+  ]
+
   constructor() { }
 
   ngOnInit() {
