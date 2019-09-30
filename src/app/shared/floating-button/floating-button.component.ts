a31d991d1a070871c145881da52edde333d6f6a4
Wagner Candido da Silva <wcsilva@latam.stefanini.com>
2019-09-30 15:24:16 -0300

    Floating button finalizado

--- src/app/shared/floating-button/floating-button.component.ts
+++ src/app/shared/floating-button/floating-button.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare const $;
+
 @Component({
   selector: 'app-floating-button',
   templateUrl: './floating-button.component.html',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FloatingButtonComponent implements OnInit {
 
+  options = [
+    { title: 'Entrada', color: '#4caf50', icon: 'attach_money', router: '' },
+    { title: 'Saída', color: '#e74938', icon: 'money_off', router: '' },
+    { title: 'Despesa Cartão', color: '#009688', icon: 'credit_card', router: '' },
+  ];
+
   constructor() { }
 
   ngOnInit() {
+    $(document).ready(function() {
+      $('[data-toggle="tooltip"]').tooltip();
+    });
   }
 
 }
