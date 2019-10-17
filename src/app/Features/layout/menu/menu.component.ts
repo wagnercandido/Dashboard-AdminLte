3e41ddd2415f047d0a3811178f5130ab96e39479
wagnercandido <wagnercssilva@gmail.com>
2019-10-17 20:33:44 -0300

    Menu Component Ts

--- src/app/Features/layout/menu/menu.component.ts
+++ src/app/Features/layout/menu/menu.component.ts
@@ -8,12 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class MenuComponent implements OnInit {
 
   options = [
-    { icon: 'home', title: 'Dashboard', router: 'principal' }
-    { icon: 'swap_vert', title: 'Transações', router: 'outro' }
-    { icon: 'account_balance', title: 'Contas', router: 'contas' }
-    { icon: 'credit_card', title: 'Cartões', router: 'cartoes' }
-    { icon: 'insert_chart_outlined', title: 'Relatórios', router: 'relatorios' }
-    { icon: 'bookmarks', title: 'Categorias', router: 'categorias' }
+    { icon: 'home', title: 'Dashboard', router: 'principal' },
+    { icon: 'swap_vert', title: 'Transações', router: 'outro' },
+    { icon: 'account_balance', title: 'Contas', router: 'contas' },
+    { icon: 'credit_card', title: 'Cartões', router: 'cartoes' },
+    { icon: 'insert_chart_outlined', title: 'Relatórios', router: 'relatorios' },
+    { icon: 'bookmarks', title: 'Categorias', router: 'categorias' },
   ]
 
   constructor() { }
